cd19fae10b47c6adb6affb0e22105d74dc75d9d5
minji0320 <rlaalswl0320@naver.com>
2022-01-18 23:42:44 +0900

    test: Cell open 상태 테스트

--- src/test/kotlin/minesweeper/domain/CellTest.kt
+++ src/test/kotlin/minesweeper/domain/CellTest.kt
@@ -11,4 +11,16 @@ internal class CellTest {
         assertThat(board[1].isMine()).isFalse
         assertThat(board[2].isMine()).isTrue
     }
+
+    @Test
+    fun `Cell의 open 상태 체크`() {
+        // 보드 상태 : 폭탄, 블록, 블록
+        val board = Board(1, 3, 1)
+        board.setMinesAndBlocks(listOf(0))
+
+        board.board[1].open()
+        assertThat(board.board[0].isOpen).isFalse
+        assertThat(board.board[1].isOpen).isTrue
+        assertThat(board.board[2].isOpen).isFalse
+    }
 }
