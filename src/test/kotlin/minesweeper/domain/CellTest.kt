af8bd6489674073fe8084ecd5e8bbfe7a9c34d6f
minji0320 <rlaalswl0320@naver.com>
2022-01-19 00:15:58 +0900

    refactor: Cell open 상태에 따른 출력 결과 테스트 수정 (랜덤으로 배정되는 Mine으로 인한 오류 수정)

--- src/test/kotlin/minesweeper/domain/CellTest.kt
+++ src/test/kotlin/minesweeper/domain/CellTest.kt
@@ -27,12 +27,18 @@ internal class CellTest {
     @Test
     fun `Cell의 open 상태에 따른 출력 결과 테스트`() {
         // 보드 상태 : 폭탄, 블록, 블록
-        val board = Board(1, 3, 1)
-        board.setMinesAndBlocks(listOf(0))
-
-        board.board[1].open()
-        assertThat(board.board[0].toString()).isEqualTo("C")
-        assertThat(board.board[1].toString()).isEqualTo("1")
-        assertThat(board.board[2].toString()).isEqualTo("C")
+        val board = Board(1, 2, 1)
+        when {
+            board.board[0].isMine() -> {
+                board.openCell(Position(0, 1))
+                assertThat(board.board[0].toString()).isEqualTo("C")
+                assertThat(board.board[1].toString()).isEqualTo("1")
+            }
+            else -> {
+                board.openCell(Position(0, 0))
+                assertThat(board.board[0].toString()).isEqualTo("1")
+                assertThat(board.board[1].toString()).isEqualTo("C")
+            }
+        }
     }
 }
